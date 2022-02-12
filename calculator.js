feea01cf115c7682b5b9f9ff5a876bebd730e7fb
nurulislamrimon <nurulislamrimon@gmail.com>
2022-02-12 14:43:23 +0600

    maded

--- calculator.js
+++ calculator.js
@@ -1,68 +1,79 @@
-const buttons = document.getElementsByClassName("btn");
-for (const button of buttons) {
-    button.addEventListener("click", function (event) {
-        const result = document.getElementById("result");
-        result.value += event.target.value;
-    });
-};
+const result = document.getElementById("result");
 const operators = document.getElementsByClassName("operator");
+const pa = document.getElementById("a");
+const pb = document.getElementById("b");
+const pr = document.getElementById("r");
+const pao = document.getElementById("ao");
 var a = '';
 var o = '';
 var b = '';
 var ao = '';
 var r = '';
+const buttons = document.getElementsByClassName("btn");
+for (const button of buttons) {
+    button.addEventListener("click", function (event) {
+
+        result.value += event.target.value;
+    });
+};
 for (const operator of operators) {
     operator.addEventListener("click", function (event) {
-        o = event.target.value;
-        const result = document.getElementById("result");
-        a = result.value;
-        // clearing
-        result.value = "";
-        // value in a and b
-        document.getElementById("a").innerText = a;
-        b = document.getElementById("b").innerText = "last number";
+        if (result.value != NaN) {
+            o = event.target.value;
+            ;
+            a = result.value;
+            // clearing
+            result.value = "";
+            // value in a and b
+            pa.innerText = a;
+        }
     });
 };
 document.getElementById("assign").addEventListener("click", function (event) {
-    const result = document.getElementById("result");
     b = result.value;
     ao = event.target.value;
-    document.getElementById("ao").innerText = ao;
-    document.getElementById("b").innerText = b;
+    pao.innerText = ao;
+    pb.innerText = b;
     if (o == "+") {
         r = parseFloat(a) + parseFloat(b);
-        document.getElementById("r").innerText = r;
-        const result = document.getElementById("result");
+        pr.innerText = r;
         result.value = r;
     }
     else if (o == "-") {
         r = parseFloat(a) - parseFloat(b);
-        document.getElementById("r").innerText = r;
-        const result = document.getElementById("result");
+        pr.innerText = r;
         result.value = r;
     }
     else if (o == "*") {
         r = parseFloat(a) * parseFloat(b);
-        document.getElementById("r").innerText = r;
-        const result = document.getElementById("result");
+        pr.innerText = r;
         result.value = r;
     }
     else if (o == "/") {
         r = parseFloat(a) / parseFloat(b);
-        document.getElementById("r").innerText = r;
-        const result = document.getElementById("result");
+        pr.innerText = r;
+        result.value = r;
+    }
+    else if (o == "/-") {
+        r = parseFloat(a) % parseFloat(b);
+        pr.innerText = parseInt(parseFloat(a) / parseFloat(b));
         result.value = r;
     }
 })
 document.getElementById("parcentage").addEventListener("click", function (event) {
-    const result = document.getElementById("result");
     b = result.value;
-    document.getElementById("b").innerText = b;
+    pb.innerText = b;
     r = parseFloat(a) * parseFloat(b) / 100;
-    document.getElementById("r").innerText = r;
+    pr.innerText = r;
     result.value = r;
 })
 document.getElementById("c").addEventListener("click", function () {
-    const result = document.getElementById("result");
+    result.value = "";
+    pa.innerText = "";
+    pb.innerText = "";
+    pao.innerText = "";
+    pr.innerText = "";
+});
+document.getElementById("del").addEventListener("click", function () {
     result.value = "";
 });
